4f6296bf90e96e9a00d166513f8bcd39b2348c3a
Ryan Hefner <hi@ryanhefner.com>
2020-03-06 13:11:27 -0500

    Add deep ommitting support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 export default function omit(obj = {}, props = []) {
   obj = {...obj};
+
   props.forEach((prop) => {
-    obj.hasOwnProperty(prop) && delete(obj[prop]);
+    const deepIndex = prop.indexOf('.');
+    if (deepIndex !== -1) {
+      const activeKey = prop.slice(0, deepIndex);
+      const activeProps = prop.slice(deepIndex + 1);
+
+      if (activeKey && obj[activeKey]) {
+        obj[activeKey] = omit(obj[activeKey], [activeProps]);
+      }
+    } else {
+      obj.hasOwnProperty(prop) && delete(obj[prop]);
+    }
   });
 
   return obj;
