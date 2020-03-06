4387b5d6cd74614c413350f6f3a05853c393246f
Ryan Hefner <hi@ryanhefner.com>
2020-03-06 12:18:32 -0500

    Update Rollup config to meet specs for new version

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 const config = {
@@ -12,11 +12,14 @@ const config = {
     format: 'umd',
     name: 'lomit',
     globals: {},
+    banner: `/*! ${pkg.name} v${pkg.version} | (c) ${new Date().getFullYear()} Ryan Hefner | ${pkg.license} License | https://github.com/${pkg.repository} !*/`,
+    footer: '/* follow me on Twitter! @ryanhefner */',
   },
   external: [],
   plugins: [
     babel({
       exclude: 'node_modules/**',
+      externalHelpers: process.env.BABEL_ENV === 'umd',
     }),
     resolve(),
     commonjs({
@@ -24,8 +27,6 @@ const config = {
     }),
     json(),
   ],
-  banner: `/*! ${pkg.name} v${pkg.version} | (c) ${new Date().getFullYear()} Ryan Hefner | ${pkg.license} License | https://github.com/${pkg.repository} !*/`,
-  footer: '/* follow me on Twitter! @ryanhefner */',
 };
 
 if (process.env.NODE_ENV === 'production') {
