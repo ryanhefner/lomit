e2515151f35b8a52f167fa2544ab127c7e080d41
Ryan Hefner <hi@ryanhefner.com>
2020-09-23 17:02:04 -0400

    Upgrade to latest Rollup plugins and settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,28 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import resolve from 'rollup-plugin-node-resolve';
-import { uglify } from 'rollup-plugin-uglify';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const config = {
   input: 'src/index.js',
   output: {
+    name: 'lomit',
     file: './index.js',
     format: 'umd',
-    name: 'lomit',
+    exports: 'named',
     globals: {},
     banner: `/*! ${pkg.name} v${pkg.version} | (c) ${new Date().getFullYear()} Ryan Hefner | ${pkg.license} License | https://github.com/${pkg.repository} !*/`,
     footer: '/* follow me on Twitter! @ryanhefner */',
   },
-  external: [],
+  external: [
+    /@babel\/runtime/,
+  ],
   plugins: [
     babel({
       exclude: 'node_modules/**',
-      externalHelpers: process.env.BABEL_ENV === 'umd',
+      babelHelpers: process.env.BABEL_ENV === 'umd' ? 'bundled' : 'runtime',
     }),
     resolve(),
     commonjs({
@@ -30,7 +33,7 @@ const config = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(uglify());
+  config.plugins.push(terser());
 }
 
 export default config;
